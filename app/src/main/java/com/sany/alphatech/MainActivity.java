50f1996e6ac7577c448f947e68f3d4c69a0a3672
sany1001 <sany11053@gmail.com>
2022-04-26 02:27:30 +0530

    init

--- app/src/main/java/com/sany/alphatech/MainActivity.java
+++ app/src/main/java/com/sany/alphatech/MainActivity.java
@@ -3,10 +3,29 @@ package com.sany.alphatech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.NoCache;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,82 +39,66 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Timer;
+
+import javax.xml.transform.ErrorListener;
 
 public class MainActivity extends AppCompatActivity {
-    String data="";
-    Handler handler=new Handler();
-    Handler mhandler=new Handler();
-    ProgressDialog progressDialog;
-    TextView display;
-    final String api_key="aio_vBGU54uGLUfVrDOX0TKUwpDEoH5i";
-    int dt=0;
+    String key= "aio_LGge72CsFbH3rCXFOamJlCmKGG9C";
+    Button fetch;
+    ArrayList<Integer> datalist;
+    ArrayAdapter<Integer> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        display=findViewById(R.id.display);
-        runnable.run();
-        display.setText("Values : "+dt);
+        fetch=findViewById(R.id.fetch);
+        datalist=new ArrayList<>();
+        adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,datalist);
+        ListView listView=findViewById(R.id.display);
+        listView.setAdapter(adapter);
+
+        RequestQueue requestQueue;
+        Cache cache=new NoCache();
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url="https://io.adafruit.com/api/v2/sanskar1001/feeds/tempfeed/data?x-aio-key="+key;
+                JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        datalist.clear();
+                        for(int i=0;i<response.length();i++)
+                        {
+                            try {
+                                JSONObject obj=response.getJSONObject(i);
+                                int data=obj.getInt("value");
+                                Log.d("data",data+"");
+                                datalist.add(data);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error.getMessage());
+                    }
+                });
+                jsonArrayRequest.setShouldCache(false);
+                requestQueue.add(jsonArrayRequest);
+            }
+        });
     }
-
-    Runnable runnable=new Runnable() {
-        @Override
-        public void run() {
-            new fetch().start();
-            mhandler.postDelayed(this,1000);
-        }
-    };
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mhandler.removeCallbacks(runnable);
-    }
-
-    class fetch extends Thread{
-        @Override
-        public void run() {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    progressDialog=new ProgressDialog(MainActivity.this);
-                    progressDialog.setMessage("Collecting data");
-                    progressDialog.setCancelable(false);
-                }
-            });
-            try {
-                URL url=new URL("https://io.adafruit.com/api/v2/sanskar1001/feeds/tempfeed/data?x-aio-key="+api_key) ;
-                HttpURLConnection cnnct= (HttpURLConnection) url.openConnection();
-                InputStream input=cnnct.getInputStream();
-                BufferedReader buffer=new BufferedReader(new InputStreamReader(input));
-                String line="";
-                while((line=buffer.readLine())!=null)
-                {
-                    data+=line;
-                }
-                if(data.length()!=0){
-                    JSONArray arr=new JSONArray(data);
-                    for(int i=0;i<arr.length();i++)
-                    {
-                        JSONObject obj=arr.getJSONObject(i);
-                        dt=obj.getInt("value");
-                        Log.d("getdata", "value: "+dt);
-                    }
-                }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    if(progressDialog.isShowing())
-                        progressDialog.dismiss();
-                }
-            });
-        }
     }
 }
--- app/src/main/java/com/sany/alphatech/SplashActivity.java
+++ app/src/main/java/com/sany/alphatech/SplashActivity.java
@@ -24,6 +24,6 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
             }
-        }, 4000);
+        }, 1000);
     }
 }
